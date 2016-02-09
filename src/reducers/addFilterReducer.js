da494d4e7108d5f08c086392fe700bd196cc95a9
carlesba <carlesba.js@gmail.com>
2016-02-09 17:11:26 +0100

    Prevent adding filters twice

--- src/reducers/addFilterReducer.js
+++ src/reducers/addFilterReducer.js
@@ -1,6 +1,10 @@
 import formsFilterer from './formsFilterer'
 
 export default function addFilterReducer (state, action) {
+  if (state.filters.indexOf(action.filter) >= 0) {
+    console.log('no add filter')
+    return state
+  }
   const newFilters = [...state.filters, action.filter]
   return Object.assign({}, state, {
     searcher: '',
