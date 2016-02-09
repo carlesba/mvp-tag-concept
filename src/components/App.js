85b1fe8e47099dda14be966868905eab93261e48
carlesba <carlesba.js@gmail.com>
2016-02-09 21:14:59 +0100

    Add icons

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,12 @@ const App = React.createClass({
   render () {
     return (
       <div className='o-vgrid'>
+        <div className='o-floated-tl'>
+          <div className='o-wrapper'><img src='/assets/logo.svg'/></div>
+        </div>
+        <div className='o-floated-tr'>
+          <div className='o-wrapper'><img src='/assets/user.svg'/></div>
+        </div>
         <div className='o-vgrid__fixed'>
           <div className='o-centerer-wrapper'>
             <SearchBox />
